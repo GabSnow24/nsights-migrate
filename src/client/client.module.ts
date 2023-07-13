d6069947505b001f13997455632b7d79b2fb3389
Gabriel Queiroz <gqueiroz_photo@hotmail.com>
2023-07-13 18:03:28 -0300

    feat: bump chart

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -3,37 +3,41 @@ import { ClientService } from './client.service';
 
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ClientsModule.register([
-      {
-        name: 'MAKIMA',
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.RABBIT_URL],
-          queue: 'engine_queue',
-          queueOptions: {
-            durable: false,
-          },
-        }
-      },
-    ]),
-    ClientsModule.register([
-      {
-        name: 'MAKIMA-TRANSCRIBE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.RABBIT_URL],
-          queue: 'sender_queue',
-          queueOptions: {
-            durable: false,
-          },
-        }
-      },
-    ]),
+    BullModule.registerQueue({
+      name: 'engine',
+    }),
+    // ClientsModule.register([
+    //   {
+    //     name: 'MAKIMA',
+    //     transport: Transport.RMQ,
+    //     options: {
+    //       urls: [process.env.RABBIT_URL],
+    //       queue: 'engine_queue',
+    //       queueOptions: {
+    //         durable: false,
+    //       },
+    //     }
+    //   },
+    // ]),
+    // ClientsModule.register([
+    //   {
+    //     name: 'MAKIMA-TRANSCRIBE',
+    //     transport: Transport.RMQ,
+    //     options: {
+    //       urls: [process.env.RABBIT_URL],
+    //       queue: 'sender_queue',
+    //       queueOptions: {
+    //         durable: false,
+    //       },
+    //     }
+    //   },
+    // ]),
 
   ],
   providers: [ClientService]
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
-import { otelSDK } from './tracing';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
+
+const actuator = require('express-actuator')
+const swStats = require('swagger-stats');
 
 const loggerInstance = new Logger('Bootstrap')
+const DEFAULT_API_PORT = 3000
 
 async function bootstrap() {
-  otelSDK.start()
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [process.env.RABBIT_URL],
-        queue: 'engine_queue',
-        queueOptions: {
-          durable: false,
-        },
-        noAck: false
-      },
-    }
-  );
-  await app.listen();
-  loggerInstance.log(`Makima Listener MS running!`)
+  const app = await NestFactory.create(AppModule);
+  app.enableCors()
+  app.use(actuator())
+  const config = new DocumentBuilder()
+    .setTitle('Makima Sender MS')
+    .setDescription('Makima Sender MS')
+    .setVersion(process.env.npm_package_version)
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.use(swStats.getMiddleware({ swaggerSpec: document }));
+
+  await app.listen(process.env.API_PORT || DEFAULT_API_PORT);
+  loggerInstance.log(`Makime Listener MS is running! Application is running on: ${await app.getUrl()}`)
 }
 
 bootstrap().catch((error) => loggerInstance.error(error))
